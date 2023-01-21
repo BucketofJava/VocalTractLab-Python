e30e517c58c6f782da7780ace1d4ced893e28bb6
BucketofJava <bucketofjava@gmail.com>
2023-01-21 16:31:04 -0600

    changed setup file

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ import shutil
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext
 
 import numpy as np
 from Cython.Build import cythonize
@@ -98,7 +99,47 @@ class Build_Backends( build_py ):
         #self.run_command( 'build_target_optimizer' )
         self.run_command( 'build_vtl' )
         build_py.run(self)
+include_dirs = []
+library_dirs = []
+libraries = []
 
+CC = os.environ.get("CC", None)
+NVCPP_EXE = CC if CC is not None and CC.endswith("nvc++") else None
+    
+if NVCPP_EXE is not None:
+    NVCPP_HOME = os.path.dirname(os.path.dirname(NVCPP_EXE))
+    include_dirs += [
+        os.path.join(NVCPP_HOME, "include-stdpar")
+    ]
+    library_dirs += [
+        os.path.join(NVCPP_HOME, "lib")
+    ]
+
+
+# noinspection PyPep8Naming
+class custom_build_ext(build_ext):
+    def run(self):
+        self.run_command('build_py')
+        build_ext.run(self);
+    def build_extensions(self):
+        if NVCPP_EXE:
+            # Override the compiler executables. Importantly, this
+            # removes the "default" compiler flags that would
+            # otherwise get passed on to nvc++, i.e.,
+            # distutils.sysconfig.get_var("CFLAGS"). nvc++
+            # does not support all of those "default" flags
+            compile_args = "-fPIC -stdpar -gpu=nordc -std=c++17"
+            link_args = "-shared -stdpar"
+            self.compiler.set_executable(
+                "compiler_so",
+                NVCPP_EXE + " " + compile_args
+            )
+            self.compiler.set_executable("compiler_cxx", NVCPP_EXE)
+            self.compiler.set_executable(
+                "linker_so",
+                NVCPP_EXE + " " + link_args
+            )
+        build_ext.build_extensions(self)
 
 
 
@@ -160,10 +201,11 @@ else:
             Extension( 'VocalTractLab.VocalTractLabApi',
                   [ './VocalTractLab/VocalTractLabApi.pyx' ],
                   language="c",
-                  libraries=[ 'VocalTractLabApi' ],
-                  library_dirs=[ './VocalTractLab/src/vocaltractlab-backend' ],# './VocalTractLab/', './VocalTractLab/VocalTractLabApi' ],
-                  include_dirs=[ np.get_include(), './VocalTractLab/src/vocaltractlab-backend' ],# './', './VocalTractLab/', './VocalTractLab/VocalTractLabApi' ],
-                  runtime_library_dirs=runtime_library_dirs #'./', './VocalTractLab/', './VocalTractLab/VocalTractLabApi' ],
+                  libraries=[ 'VocalTractLabApi' ]+libraries,
+                  library_dirs=[ './VocalTractLab/src/vocaltractlab-backend' ]+library_dirs,# './VocalTractLab/', './VocalTractLab/VocalTractLabApi' ],
+                  include_dirs=[ np.get_include(), './VocalTractLab/src/vocaltractlab-backend' ]+include_dirs,# './', './VocalTractLab/', './VocalTractLab/VocalTractLabApi' ],
+                  runtime_library_dirs=runtime_library_dirs+library_dirs,
+                  extra_compile_args=["-std=c++17"] #'./', './VocalTractLab/', './VocalTractLab/VocalTractLabApi' ],
                   )
         ]
     )
@@ -203,6 +245,7 @@ Operating System :: Unix
 cmdclass = dict( #build_target_optimizer = Build_Target_Optimizer,
                  build_vtl = Build_VTL,
                  build_py = Build_Backends,
+                 build_ext=custom_build_ext
                  )
 #cmdclass['build_target_optimizer'] = Build_Target_Optimizer
 #cmdclass['build_vtl'] = Build_VTL
